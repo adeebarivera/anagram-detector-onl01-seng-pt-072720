3029f6aad31130110c0698ee7df8d147fe553b02
adeeba reaz <adeebarivera@outlook.com>
2020-08-19 12:03:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,12 @@
 class Anagram
    attr_accessor :name 
   
-  def initialize(word)
+def initialize(word)
     @name = word 
-  end
+end
 
+def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
